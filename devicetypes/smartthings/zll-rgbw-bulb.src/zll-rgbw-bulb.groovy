84def2988fca4f0954b674506ea61726cfcca97d
m1kegibson <37812067+m1kegibson@users.noreply.github.com>
2019-09-06 19:43:05 +0100

    Update zll-rgbw-bulb.groovy
    Added fingerprint for OSRAM/LEDVANCE CLA60 RGBW Z3.  Bulbs are boxed as Osram Smart+ Classic E27 Multicolor but when pairing with zigbee have manufacturer LEDVANCE and model CLA60 RGBW Z3.

--- devicetypes/smartthings/zll-rgbw-bulb.src/zll-rgbw-bulb.groovy
+++ devicetypes/smartthings/zll-rgbw-bulb.src/zll-rgbw-bulb.groovy
@@ -51,6 +51,7 @@ metadata {
         fingerprint profileId: "C05E", inClusters: "0000,0003,0004,0005,0006,0008,0300,1000", outClusters: "0019", manufacturer: "Philips", model: "LST001", deviceJoinName: "Philips Hue Lightstrip"
         fingerprint profileId: "C05E", inClusters: "0000,0003,0004,0005,0006,0008,0300,1000", outClusters: "0019", manufacturer: "Philips", model: "LST002", deviceJoinName: "Philips Hue Lightstrip"
         fingerprint profileId: "C05E", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0300", outClusters: "0019", manufacturer: "innr", model: "RB 185 C", deviceJoinName: "innr Smart Bulb RGBW"
+        fingerprint profileId: "C05E", inClusters: "0000,0003,0004,0005,0006,0008,0300,0B05,1000,FC0F", outClusters: "0019", "manufacturer":"LEDVANCE", "model":"CLA60 RGBW Z3", deviceJoinName:"OSRAM/LEDVANCE CLA60 RGBW Z3"
     }
 
     // UI tile definitions
